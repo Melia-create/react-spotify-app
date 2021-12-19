be7107651b2e034e3030b9008a2aba37e7bc6823
Amelia <amelia.riding@sofology.co.uk>
2021-12-19 18:35:14 +0000

    Added Components

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div>
+            <h2 className="search-header">Spotify Search</h2>
+        </div>
+    )
+}
---
+++ src/components/RedirectPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function RedirectPage() {
+    return (
+        <div>
+            <h2>Redirect Page</h2>
+        </div>
+    )
+}
