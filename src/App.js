36956182622a6f57d71cf06ea4196d9103e2692b
Amelia <amelia.riding@sofology.co.uk>
2021-12-20 12:03:56 +0000

    Login Page and basic styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-
+import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
      <h2>HELLO WORLD!</h2>
+     <Login/>
     </div>
   );
 }
