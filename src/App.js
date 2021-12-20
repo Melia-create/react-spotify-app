5cb12abb08d3c9dcdffca21431c4bc54d632dba7
Amelia <amelia.riding@sofology.co.uk>
2021-12-20 12:52:18 +0000

    Sorted access token

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-import Login from './components/Login'
+import Login from './components/Login';
+import { getTokenFromUrl } from './config/spotify'
 
 function App() {
+  [token, setToken] = useState()
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+
+    console.log("token", token);
+
+  }, []);
+
   return (
     <div className="App">
-     <h2>HELLO WORLD!</h2>
-     <Login/>
+      {token ?
+        <h1>Logged In</h1>
+        :
+        <Login />
+      }
     </div>
   );
 }
